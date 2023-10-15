999823f9c734ef88e9aff4bdd2c6ed1b21bf669c
iaraoliveira <iara99oliveira@gmail.com>
2023-10-15 12:34:52 -0300

    validação de datas da task

--- src/main/java/br/com/iaraoliveira/todolist/task/TaskController.java
+++ src/main/java/br/com/iaraoliveira/todolist/task/TaskController.java
@@ -1,5 +1,6 @@
 package br.com.iaraoliveira.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +22,21 @@ public class TaskController {
     private iTaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de início / término deve ser maior do que a data atual");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de início deve ser maior do que a data de termino");
+        }
         var taskCreated = this.taskRepository.save(taskModel);
-        return taskCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
     }
 }
